02f1dd674abe1f49821e04923b8789a834759fe7
Renan <renanm.b3@gmail.com>
2022-06-07 16:33:04 -0500

    first commit

---
+++ my_site/my_app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+def example_view(request):
+    # my_app/templates/my_app/example.html
+    return render(request,'my_app/example.html')
+
+def variable_view(request):
+
+    my_var = {'first_name':'rosalind',
+    'last_name':'Franklin', 
+    'some_list':[1,2,3], 
+    'some_dict':{'inside_key':'inside_value'},
+    'user_logged_in':False
+    }
+
+    return render(request, 'my_app/variable.html', context=my_var)
