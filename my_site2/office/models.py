697442868db90cda836bcfe780849059e831c4e5
Renan <renanm.b3@gmail.com>
2022-06-09 22:12:41 -0500

    Update models.py

--- my_site2/office/models.py
+++ my_site2/office/models.py
@@ -6,6 +6,6 @@ class Patient(models.Model):
     last_name = models.CharField(max_length=30)
     age = models.IntegerField()
 
-def __str__(self):
+    def __str__(self):
 
-    return f"{self.last_name}, {self.first_name} is {self.age} years old."
+        return f"{self.last_name}, {self.first_name} is {self.age} years old."
