329f647654f94e8d3054e23ee2ed945fd573c39e
Renan <renanm.b3@gmail.com>
2022-06-14 21:39:30 -0500

    a

--- library/library/urls.py
+++ library/library/urls.py
@@ -21,5 +21,15 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
-    path('', RedirectView.as_view(url='catalog'))
+    path('', RedirectView.as_view(url='catalog')),
+    path('accounts/',include('django.contrib.auth.urls'))
 ]
+
+# accounts/ login/ [name='login']
+# accounts/ logout/ [name='logout']
+# accounts/ password_change/ [name='password_change']
+# accounts/ password_change/done/ [name='password_change_done']
+# accounts/ password_reset/ [name='password_reset']
+# accounts/ password_reset/done/ [name='password_reset_done']
+# accounts/ reset/<uidb64>/<token>/ [name='password_reset_confirm']
+# accounts/ reset/done/ [name='password_reset_complete']
